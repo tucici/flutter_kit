8963dcfdbb99381e586c71de5699f5544efab3b5
tucici <290255613@qq.com>
2024-10-15 09:11:34 +0800

    no message

--- lib/flutter_kit.dart
+++ lib/flutter_kit.dart
@@ -5,7 +5,7 @@ export 'package:flutter_kit_dsp/flutter_kit_dsp.dart';
 export 'package:flutter_kit_extension/flutter_kit_extension.dart';
 export 'package:flutter_kit_file/flutter_kit_file.dart';
 export 'package:flutter_kit_http/flutter_kit_http.dart';
-export 'package:flutter_kit_hud/flutter_kit_hud.darta';
+export 'package:flutter_kit_hud/flutter_kit_hud.dart';
 export 'package:flutter_kit_nfc/flutter_kit_nfc.dart';
 export 'package:flutter_kit_ota/flutter_kit_ota.dart';
 export 'package:flutter_kit_ui/flutter_kit_ui.dart';
