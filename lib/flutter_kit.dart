ed7de6aa539085e081952da295e80d00afe40c3a
tucici <290255613@qq.com>
2024-10-17 18:10:35 +0800

    no message

--- lib/flutter_kit.dart
+++ lib/flutter_kit.dart
@@ -1,24 +1,15 @@
 library flutter_kit;
 
 export 'package:flutter_kit_bluetooth/flutter_kit_bluetooth.dart';
+export 'package:flutter_kit_call/flutter_kit_call.dart';
 export 'package:flutter_kit_dsp/flutter_kit_dsp.dart';
 export 'package:flutter_kit_extension/flutter_kit_extension.dart';
 export 'package:flutter_kit_file/flutter_kit_file.dart';
 export 'package:flutter_kit_http/flutter_kit_http.dart';
 export 'package:flutter_kit_hud/flutter_kit_hud.dart';
+export 'package:flutter_kit_map/flutter_kit_map.dart';
 export 'package:flutter_kit_nfc/flutter_kit_nfc.dart';
 export 'package:flutter_kit_ota/flutter_kit_ota.dart';
-export 'package:flutter_kit_ui/flutter_kit_ui.dart';
-export 'package:flutter_kit_call/flutter_kit_call.dart';
-export 'package:flutter_kit_map/flutter_kit_map.dart';
 export 'package:flutter_kit_permission/flutter_kit_permission.dart';
 export 'package:flutter_kit_share/flutter_kit_share.dart';
-
-/// A Calculator.
-class flutter_kitTest {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-  void log() {
-    print('测试测试ss');
-  }
-}
+export 'package:flutter_kit_ui/flutter_kit_ui.dart';
