932eb514e435670577c697a313fa8771b3ad214f
tucici <290255613@qq.com>
2024-12-24 18:19:15 +0800

    update

--- lib/flutter_kit.dart
+++ lib/flutter_kit.dart
@@ -2,6 +2,7 @@ library flutter_kit;
 
 export 'package:flutter_kit_bluetooth/flutter_kit_bluetooth.dart';
 export 'package:flutter_kit_call/flutter_kit_call.dart';
+export 'package:flutter_kit_database/flutter_kit_database.dart';
 export 'package:flutter_kit_dsp/flutter_kit_dsp.dart';
 export 'package:flutter_kit_extension/flutter_kit_extension.dart';
 export 'package:flutter_kit_file/flutter_kit_file.dart';
@@ -14,3 +15,4 @@ export 'package:flutter_kit_ota/flutter_kit_ota.dart';
 // export 'package:flutter_kit_permission/flutter_kit_permission.dart';
 export 'package:flutter_kit_share/flutter_kit_share.dart';
 export 'package:flutter_kit_ui/flutter_kit_ui.dart';
+export 'package:flutter_kit_ui_common/flutter_kit_ui_common.dart';
